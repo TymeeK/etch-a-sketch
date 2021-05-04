042259c5d8979c64b760f125cf3f14144182aea3
Tymee Kong <tymeekong562@gmail.com>
2021-05-03 18:13:01 -0700

    Added checkMouseEnter, changeColor, buttonPress, generateColor, and generateOpacity functions

--- script.js
+++ script.js
@@ -1,20 +1,53 @@
 function init() {
     createGrid();
+    checkMouseEnter();
+    buttonPress();
 }
 
 function createGrid(row = 16, col = 16) {
     const containerDiv = document.querySelector("#container");
     
-    containerDiv.style.cssText = "display: grid;"
-    + `grid-template-columns: repeat(${col}, 1fr); grid-template-columns: repea`
-    + `t(${row}, 1fr);`;
+    containerDiv.style.cssText = 
+    `grid-template-columns: repeat(${col}, 1fr);` +
+    `grid-template-columns: repeat(${row}, 1fr);`;
     
     for (let i = 0; i < row * col; i++) {
         const gridDivs = document.createElement("div");
-        gridDivs.innerText = i + 1;
         gridDivs.className = "innerDiv";
         containerDiv.appendChild(gridDivs);
     }
+}
+
+function checkMouseEnter() {
+    const gridDivs = document.querySelectorAll(".innerDiv");
+    gridDivs.forEach(button => {
+        button.addEventListener("mouseenter", changeColor);
+    });
+}
+
+function changeColor(e) {
+    e.target.style.cssText = 
+    `background-color: rgba(${generateColor()}, ${generateColor()}, 
+    ${generateColor()}, ${generateOpacity()});`;
+   
+}
+
+function buttonPress() {
+    const button = document.getElementById("button-grid");
+    button.addEventListener("click", () => {
+        console.log(button);
+
+    });
+}
+
+function generateColor() {
+    const color = Math.floor(Math.random() * 256);
+    return color;
+}
+
+function generateOpacity() {
+    const opacity = Math.random(); 
+    return opacity;
 }
 
 init();
